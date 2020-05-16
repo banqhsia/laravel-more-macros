d231ffa9a45d5a3f74cff7ba5b57bcc1f448b99c
Benyi <banqhsia@msn.com>
2020-05-16 16:12:39 +0800

    Handle illuminate support classes not existing

--- src/methods.php
+++ src/methods.php
@@ -1,21 +1,27 @@
 <?php
 
-use \Illuminate\Support\Arr;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 
-Arr::macro('absent', function ($array, $keys) {
-    return ! static::has($array, $keys);
-});
+if (class_exists(\Illuminate\Support\Arr::class)) {
+    Arr::macro('absent', function ($array, $keys) {
+        return ! static::has($array, $keys);
+    });
+}
 
-Carbon::macro('recognized', function ($time = null, $tz = null) {
-    return (bool) @static::parse($time, $tz);
-});
+if (class_exists(\Illuminate\Support\Carbon::class)) {
+    Carbon::macro('recognized', function ($time = null, $tz = null) {
+        return (bool) @static::parse($time, $tz);
+    });
 
-Carbon::macro('unrecognized', function ($time = null, $tz = null) {
-    return ! static::recognized(...func_get_args());
-});
+    Carbon::macro('unrecognized', function ($time = null, $tz = null) {
+        return ! static::recognized(...func_get_args());
+    });
+}
 
-Collection::macro('diffCi', function ($diffBy) {
-    return $this->diffUsing($diffBy, 'strcasecmp');
-});
+if (class_exists(\Illuminate\Support\Collection::class)) {
+    Collection::macro('diffCi', function ($diffBy) {
+        return $this->diffUsing($diffBy, 'strcasecmp');
+    });
+}
